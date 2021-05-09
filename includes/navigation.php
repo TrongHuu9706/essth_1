e731f39753fd411329732351e4952c7d5903dfa8
TrongHuu9706 <79493505+TrongHuu9706@users.noreply.github.com>
2021-05-09 17:24:03 +0700

    Update navigation.php

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,12 +1,12 @@
 <nav id="menu-nav-wrap">
    	<h3>Danh sách</h3>   	
 		<ul class="nav-list">
-			<li><a href="../index.php" title="">Trang chủ</a></li>
-			<li><a href="../controllers/product.php" title="">Sản phẩm</a></li>
-			<li><a href="../controllers/document.php" title="">Tài liệu bán hàng</a></li>
-			<li><a href="../controllers/record.php" title="">Ghi âm</a></li>					
-			<li><a href="../controllers/buudien.php" title="">Bưu điện</a></li>
+			<li><a href="" title="">Trang chủ</a></li>
+			<li><a href="" title="">Sản phẩm</a></li>
+			<li><a href="" title="">Tài liệu bán hàng</a></li>
+			<li><a href="" title="">Ghi âm</a></li>					
+			<li><a href="" title="">Bưu điện</a></li>
 			<li><a href="#" title="">Hỗ trợ</a></li>	
-			<li><a href="../users/" title="">Đăng nhập</a></li>		
+			<li><a href="" title="">Đăng nhập</a></li>		
 		</ul>
 	</nav> <!-- /menu-nav-wrap -->
