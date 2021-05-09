94146bddfc2d123c2d96b9185c9609ce7809115c
TrongHuu9706 <79493505+TrongHuu9706@users.noreply.github.com>
2021-05-09 17:23:22 +0700

    Delete views directory

--- views/includes/footer.php
+++
@@ -1,8 +0,0 @@
-<!-- Footer -->
-<footer class="py-5 bg-dark">
-      <div class="container">
-        <p class="m-0 text-center text-white"><span>© Copyright 2021 ESSTH All Rights Reserved | </span> 
-          <span>Design by <a href="https://www.facebook.com/appshop.hr.7">ESSTH</a></span></p>
-      </div>
-      <!-- /.container -->
-    </footer>
--- views/includes/sidebar-doc.php
+++
@@ -1,8 +0,0 @@
-<div class="col-lg-3">
-<h1 class="my-4"><a href="../controllers/document.php">Tài Liệu</h1></a>
-<div class="list-group">
-<a href="document-CLBH.php" class="list-group-item">Chiến Lược Bán Hàng</a>
-<a href="document-QTTV.php" class="list-group-item">Quy Trình Tư Vấn</a>
-<a href="document-TCN.php" class="list-group-item">Thưởng Cá Nhân</a>
-<a href="document-IN-QTTV.php" class="list-group-item">Quy Trình Tư Vấn Phòng IN<b style="color:red;">*</b></a>
-</div>
